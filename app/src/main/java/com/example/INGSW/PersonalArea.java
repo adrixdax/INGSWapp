07f3427366811816a69fb32429a7f7aea68652a1
tankee <tankextreme@gmail.com>
2021-01-27 23:04:19 +0100

    fixato l'aggiornamento della propic nel RegistrationScreen e anche il relativo aggiornamento dell' url.

---
+++ app/src/main/java/com/example/INGSW/PersonalArea.java
@@ -0,0 +1,14 @@
+package com.example.INGSW;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalArea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_area);
+    }
+}
