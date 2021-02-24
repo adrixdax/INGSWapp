5460a4a74735e0ecb0eba4c079a187ee4918a089
tankee <tankextreme@gmail.com>
2021-02-24 18:47:02 +0100

    Added working logout button and fixed issues on Loading user infos on Profile Screen -Antonio

--- app/src/main/java/com/example/INGSW/PersonalArea.java
+++ app/src/main/java/com/example/INGSW/PersonalArea.java
@@ -3,15 +3,22 @@ package com.example.INGSW;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
@@ -25,10 +32,11 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PersonalArea extends Fragment {
 
-    private String textname;
+    private Button logout;
 
     private FirebaseUser mFirebaseUser;
     private DatabaseReference reference;
+    GoogleSignInAccount acct;
 
     private String userID;
     final String propic = "";
@@ -37,23 +45,26 @@ public class PersonalArea extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
 
 
-
         View root = inflater.inflate(R.layout.personal_area, container, false);
 
         mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference("Users");
-        userID = mFirebaseUser.getUid();
 
+
+        logout = (Button) root.findViewById(R.id.Logout_button);
         final TextView nicknameView = (TextView) root.findViewById(R.id.personal_profile_nick);
         final TextView mailView = (TextView) root.findViewById(R.id.personal_profile_mail);
         final CircleImageView propicView = (CircleImageView) root.findViewById(R.id.personal_profile_image);
 
-        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+
+        if (mFirebaseUser != null) {
+            userID = mFirebaseUser.getUid();
+            reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 User userProfile = snapshot.getValue(User.class);
 
-                if(userProfile != null){
+                if (userProfile != null) {
                     String nickname = userProfile.nickname;
                     String mail = userProfile.email;
                     String propic = userProfile.propic;
@@ -69,8 +80,32 @@ public class PersonalArea extends Fragment {
 
             }
         });
+    }else{
+            acct = GoogleSignIn.getLastSignedInAccount(PersonalArea.this.getContext());
+            if(acct != null){
+                String nickname = acct.getDisplayName();
+                String mail = acct.getEmail();
+                String propic = acct.getPhotoUrl().toString();
+
+                nicknameView.setText(nickname);
+                mailView.setText(mail);
+                Glide.with(root.getContext()).load(propic).into(propicView);
+            }
+
+        }
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent logoutIntent = new Intent(PersonalArea.this.getActivity(),LoginScreen.class);
+                logoutIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(logoutIntent);
+            }
+        });
 
         return root;
-
     }
+
+
 }
